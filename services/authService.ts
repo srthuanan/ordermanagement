28e68ace1106efd2232e465896e61a76b6e4be1d
srthuanan <showroomthuanan@gmail.com>
2025-10-10 13:53:52 +0700

    Sửa

--- services/authService.ts
+++ services/authService.ts
@@ -10,7 +10,7 @@ export const userToConsultantMap: Record<string, string> = {
     'tannd': 'NGUYỄN DUY TÂN', 'ducdt': 'ĐINH TÀI ĐỨC', 'dattt': 'TỐNG THÀNH ĐẠT', 'haink': 'NGUYỄN KIM HẢI',
     'xuyennb': 'NGUYỄN BẢO XUYÊN', 'diennt': 'NGUYỄN THỊ DIỆN', 'trungnm': 'NGUYỄN MINH TRUNG', 'cant': 'NGUYỄN THANH CẢ',
     'tienna': 'NGUYỄN ANH TIẾN', 'namlv': 'LÊ VĂN NAM', 'tramhdt': 'HUỲNH DIỆP THANH TRÂM', 'thanhldv': 'LÊ DƯ VĂN THÀNH', 'chinhtc': 'TRANG CÔNG CHÍNH',
-    'lanvt': 'VÕ THẾ LÂN', 'vyny': 'NGUYỄN THỊ YẾN VY', 'vynty': 'NGUYỄN THỊ YẾN VY', 'huyhkn': 'NGUYỄN HOÀNG KHANG HUY',
+    'lanvt': 'VÕ THẾ LÂN', 'vynty': 'NGUYỄN THỊ YẾN VY', 'huyhkn': 'NGUYỄN HOÀNG KHANG HUY',
     'thanhdn': 'ĐỒNG NGỌC THÀNH',
 };
 
@@ -49,7 +49,6 @@ export const userRoleMap: Record<string, string> = {
     'thanhldv': 'Tư vấn bán hàng',
     'chinhtc': 'Tư vấn bán hàng',
     'lanvt': 'Tư vấn bán hàng',
-    'vyny': 'Tư vấn bán hàng',
     'vynty': 'Tư vấn bán hàng',
     'huyhkn': 'Tư vấn bán hàng',
     'thanhdn': 'Tư vấn bán hàng',
