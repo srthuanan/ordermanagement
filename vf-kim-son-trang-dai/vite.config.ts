db4540ca01e5dde0e530f9084f7976ea8683d290
srthuanan <showroomthuanan@gmail.com>
2026-05-15 10:56:15 +0700

    Fix Vite build config

--- vf-kim-son-trang-dai/vite.config.ts
+++ vf-kim-son-trang-dai/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: fileURLToPath(new URL('./node_modules/react', import.meta.url)),
-      'react-dom': fileURLToPath(new URL('./node_modules/react-dom', import.meta.url)),
-    }
-  },
   server: {
     port: 5174
   },
