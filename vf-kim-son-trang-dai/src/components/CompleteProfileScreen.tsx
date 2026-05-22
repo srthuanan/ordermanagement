f671fcc5141abfc672c9d7313137e2c3832a80ab
srthuanan <showroomthuanan@gmail.com>
2026-05-22 14:49:53 +0700

    fix(ui): use correct path for supabaseClient in CompleteProfileScreen

--- vf-kim-son-trang-dai/src/components/CompleteProfileScreen.tsx
+++ vf-kim-son-trang-dai/src/components/CompleteProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../services/supabaseClient';
 import { ProfileRow } from '../types';
 
 interface Props {
